f54c3d3a2f61fd522e91a3dbd5257938ae174f77
navya457 <61045125+navya457@users.noreply.github.com>
2020-06-04 18:16:48 +0530

    Update Dataprovider.java

--- src/test/java/utils/Dataprovider.java
+++ src/test/java/utils/Dataprovider.java
@@ -5,12 +5,12 @@ import java.lang.reflect.Method;
 import org.testng.annotations.DataProvider;
 
 public class Dataprovider {
-//@DataProvider(name="message")
-//public static Object[][] launchapp(Method m)
-//{
-//	ReadExcel ex=new ReadExcel("");
-//	System.out.println(m.getName());
-//	String name=m.getName();
-//	return 
-//}
+@DataProvider(name="message")
+public static Object[][] launchapp(Method m)
+{
+	ReadExcel ex=new ReadExcel("");
+	System.out.println(m.getName());
+	String name=m.getName();
+	return 
+}
 }
