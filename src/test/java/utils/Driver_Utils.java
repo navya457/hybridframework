b559960f19e69f3a8f7fec3813958aca1bf15794
navya457 <61045125+navya457@users.noreply.github.com>
2020-06-04 18:09:03 +0530

    Update Driver_Utils.java

--- src/test/java/utils/Driver_Utils.java
+++ src/test/java/utils/Driver_Utils.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidElement;
 
-public class Driver_Utils extends CommonConfig {
+public class DriverUtils extends CommonConfig {
 	Boolean check=false;
 	
 	public void clickOnAllowbtn()
@@ -18,15 +18,6 @@ public class Driver_Utils extends CommonConfig {
 			ta.tap(driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button"))).perform();
 		
 			
-	//		if(isElementExist(Locator_Driver.allowbtn))
-//			{
-//				System.out.println("clickallowE");
-//				clickBtn(Locator_Driver.allowbtn);
-//				System.out.println("click allow");
-//			}
-//			else {
-//				Assert.assertFalse(check);
-//			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -46,22 +37,21 @@ public class Driver_Utils extends CommonConfig {
 			
 		}
 		catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
 	}
 	
-	public void register_Page()
+	public void registerPage()
 	{
 		try {
 			enterTextToInputField(Locator_Driver.first_Name, "navya");
 			enterTextToInputField(Locator_Driver.last_Name, "shree");
 			enterTextToInputField(Locator_Driver.mobile_Number, "8900123890");
 			enterTextToInputField(Locator_Driver.Email_Id, "navi@gmail.com");
-			//enterTextToInputField(Locator_Driver, "");
+			
 		}
 		catch (Exception e) {
-			// TODO: handle exception
+			
 		}
 	}
 
